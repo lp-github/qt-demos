11f761743d65a6b46011634f4f7151a448d8c60a
lp-github <3144461554@qq.com>
2019-07-10 22:24:43 +0800

    add shapewidget and v4l2

---
+++ ShapeWidget/main.cpp
@@ -0,0 +1,12 @@
+#include <QtGui/QApplication>
+#include "shapewidget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Q_INIT_RESOURCE(res);
+    ShapeWidget w;
+    w.show();
+    return a.exec();
+}
+
